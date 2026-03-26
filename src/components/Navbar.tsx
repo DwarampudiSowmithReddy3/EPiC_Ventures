e0186eed800f0f721eb001ede58a9ef9c8151f85
Dwarampudi Sowmith Reddy <dsowmithreddy@gmail.com>
2026-03-26 10:30:28 +0530

    Update mobile navigation and add Satya Reddy to leadership section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,9 @@ const Navbar = () => {
 
   const handleClick = (href: string) => {
     setMobileOpen(false);
-    document.querySelector(href)?.scrollIntoView({ behavior: "smooth" });
+    setTimeout(() => {
+      document.querySelector(href)?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
   };
 
   return (
