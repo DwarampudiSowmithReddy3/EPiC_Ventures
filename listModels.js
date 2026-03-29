e55059c6ab5f47c69031146abbff8753d1b9830e
Dwarampudi Sowmith Reddy <dsowmithreddy@gmail.com>
2026-03-29 23:29:02 +0530

    chore: remove accidental api key file

--- listModels.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = "AIzaSyC3P4tSgi8qcWXC6Q4G6A4PuOJO3VJnlUI";
-fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => console.log(data.models.map(m => m.name).join("\n")))
-  .catch(console.error);
